cf36b780a80ecc85078c2531f5e5c47599970412
AzahraSalsaa <azahras751@gmail.com>
2024-04-26 17:47:27 +0700

    js6

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="card card-outline card-primary">
     <div class="card-header">
-        <h3 class="card-title">{{ $page->title }}</h3>
+        <h3 class="card-title"></h3>
         <div class="card-tools">
             <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}">Tambah</a>
         </div>
